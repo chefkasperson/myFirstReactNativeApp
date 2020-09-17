d77faf26e0cbc0516bffba439ba698e4d66407bd
chefkasperson <chefkasperson@gmail.com>
2020-09-17 13:22:21 -0700

    updated presshandler to remove item on press

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -13,8 +13,16 @@ export default function App() {
     {name: 'bowser', id: '7'}
   ])
   
+  const pressHandler = (id) => {
+    console.log(id)
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id !== id)
+    })
+  }
 
   return (
+
+
     <View style={styles.container}>
 
     <FlatList 
@@ -22,7 +30,9 @@ export default function App() {
       keyExtractor={(item) => item.id}
       data={people}
       renderItem={({ item }) => (
-        <Text style={styles.item}>{item.name}</Text>
+        <TouchableOpacity onPress={() => pressHandler(item.id)}>
+          <Text style={styles.item}>{item.name}</Text>
+        </TouchableOpacity>
       )}
     />
   
