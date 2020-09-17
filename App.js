4e9abf02240fa8904caef782f509ec2e98d74ae6
chefkasperson <chefkasperson@gmail.com>
2020-09-16 23:15:53 -0700

    added person object to state and updated button to toggle to a different object

--- App.js
+++ App.js
@@ -4,15 +4,17 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('Erik')
+  const [person, setPerson] = useState({name: "Isla", age: 2})
   const clickHandler = () => {
     name === "Erik" ? setName("Harold") : setName("Erik")
+    person.name === "Isla" ? setPerson({name: "Tais", age: 38}) : setPerson({name: "Isla", age: 2})
   }
   return (
     <View style={styles.container}>
      <Text>My name is {name}</Text>
-     <Text></Text>
+     <Text>Her name is {person.name} and she is {person.age} years old</Text>
      <View style={styles.buttonContainer}>
-      <Button title="update name" onPress={clickHandler}/>
+      <Button title="update state" onPress={clickHandler}/>
      </View>
     </View>
   );
