b5a6d758b1dfd824e809f8154a8f8208f7d4fbf4
chefkasperson <chefkasperson@gmail.com>
2020-09-17 12:55:51 -0700

    added a flatlist component to try it out

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState("Erik")
-  const [age, setAge] = useState(39)
-
-  const handleNameChange = (val) => setName(val)
-  const handleAgeChange = (val) => setAge(val)
+  const [people, setPeople] = useState([
+    {name: 'shaun', key: '1'},
+    {name: 'yoshi', key: '2'},
+    {name: 'mario', key: '3'},
+    {name: 'luigi', key: '4'},
+    {name: 'peach', key: '5'},
+    {name: 'toad', key: '6'},
+    {name: 'bowser', key: '7'}
+  ])
+  
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput
-       style={styles.input}
-       placeholder='e.g. JohnDoe'
-       onChangeText={handleNameChange}
-       />
-      <Text>Enter Age:</Text>
-      <TextInput
-       style={styles.input}
-       placeholder='e.g. 53'
-       onChangeText={handleAgeChange}
-       />
-      <Text>name: {name}, age: {age} </Text>
+
+    <FlatList 
+      data={people}
+      renderItem={({ item }) => (
+        <Text style={styles.item}>{item.name}</Text>
+      )}
+    />
+  
+    <ScrollView>
+    {people.map((item) => {
+        return (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )
+      })}
+      </ScrollView>
+
+
+      
     </View>
   );
 }
@@ -32,17 +44,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor:'#777',
-    backgroundColor: '#eee',
-    color: '#444',
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
 
   }
 });
