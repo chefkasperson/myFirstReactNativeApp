e2134eb17689b496e6a1613635e59c5bc8a49319
chefkasperson <chefkasperson@gmail.com>
2020-09-18 01:22:43 -0700

    began to outile todo list

--- App.js
+++ App.js
@@ -3,10 +3,20 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
-
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key='1' },
+    { text: "create an app", key='2' },
+    { text: "play on the switch", key='3' }
+  ])
   return (
     <View style={styles.container} >
-    
+      {/* Header */}
+      <View style={styles.content}>
+        {/* TODO form */}
+        <View style={styles.list} >
+          <FlatList />
+        </View>
+      </View>
     </View>
   );
 }
